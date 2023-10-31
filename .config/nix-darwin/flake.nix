29f08b277caded8b3af3a996b82958c690937970
Beatrix Klebe <bklebe@mbta.com>
2023-10-31 11:47:23 -0400

    fix: use aarch64-darwin

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -33,7 +33,7 @@
         system.stateVersion = 4;
 
         # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "x86_64-darwin";
+        nixpkgs.hostPlatform = "aarch64-darwin";
       };
     in {
       # Build darwin flake using:
