879c90812c2d19c85d8d5f1c24271341fe792eeb
Beatrix Klebe <beatrixk@hey.com>
2023-11-08 10:44:57 -0500

    feat: enable Lua support in Emacs

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -140,7 +140,7 @@
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       lua               ; one-based indices? one-based indices
        (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        (nix +lsp +tree-sitter)               ; I hereby declare "nix geht mehr!"
