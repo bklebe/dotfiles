956f577d83cb7a8bb69e33523b4f824174deeb56
Beatrix Klebe <bklebe@mbta.com>
2024-06-18 13:12:17 -0400

    chore: add devenv.lock and comment out demos

--- devenv.nix
+++ devenv.nix
@@ -2,6 +2,8 @@
 
 {
   cachix.enable = false;
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
 
   # https://devenv.sh/packages/
   packages = [
@@ -28,12 +30,19 @@
     pkgs.shfmt
   ];
 
-  enterShell = ''
-  '';
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+  # enterShell = ''
+  #   hello
+  #   git --version
+  # '';
 
   # https://devenv.sh/tests/
-  enterTest = ''
-  '';
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep "2.42.0"
+  # '';
 
   # https://devenv.sh/services/
   # services.postgres.enable = true;
