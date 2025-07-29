5e61182957d1c693438498cd2cbc9c94f34e71fe
Beatrix Klebe <beatrix.klebe@goswift.ly>
2025-07-29 16:34:01 -0700

    feat: gradle + jdk stable paths

--- home.nix
+++ home.nix
@@ -158,6 +158,16 @@ in
   };
   xdg.enable = true;
 
+  xdg.dataFile.my-gradle-install = {
+    source = "${pkgs.gradle}/lib/gradle";
+    recursive = true;
+  };
+
+  xdg.dataFile.my-gradle-jdk = {
+    source = pkgs.gradle.jdk.home;
+    recursive = true;
+  };
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
