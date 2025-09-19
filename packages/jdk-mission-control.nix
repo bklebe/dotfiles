cd90b606e6819e950ba68ff53126aecf2902c8d9
Beatrix Klebe <beatrixk@hey.com>
2025-09-19 09:16:15 -0400

    feat: jdk mission control derivation

---
+++ packages/jdk-mission-control.nix
@@ -0,0 +1,39 @@
+{ pkgs }:
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "jdk-mission-control";
+  version = "9.1.1";
+
+  src = pkgs.fetchurl {
+    url = "https://github.com/adoptium/jmc-build/releases/download/${version}/org.openjdk.jmc-${version}-macosx.cocoa.aarch64.tar.gz";
+    sha256 = "";
+  };
+
+  nativeBuildInputs = [ ];
+
+  buildInputs = [ pkgs.jdk21 ];
+
+  unpackPhase = ''
+    cat $src | tar xv -
+  '';
+
+  installPhase = ''
+    mkdir -p $out/Applications
+    mv "JDK Mission Control.app" $out/Applications/
+
+    # Patch zmc.ini to use Nix Java
+    iniFile="$out/Applications/JDK Mission Control.app/Contents/Eclipse/jmc.ini"
+    if [ -f "$iniFile" ]; then
+      # Insert --vm line before -vmargs
+      sed -i '/^-vmargs/i\-vm\n${pkgs.jdk21}/bin' "$iniFile"
+    fi
+  '';
+
+  meta = with pkgs.lib; {
+    description = "Eclipse Mission Control is a low-overhead profiling and diagnostics tools suite for the JVM.";
+    homepage = "https://adoptium.net/jmc";
+    platforms = platforms.darwin;
+    maintainers = [ ];
+    license = licenses.upl;
+  };
+}
